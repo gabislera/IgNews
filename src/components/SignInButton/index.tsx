b846e5aebdc1681b4efee8b0f1537b804526a30c
Gabriela Carniel <gabiicarniel@gmail.com>
2023-06-13 12:31:13 -0300

    addstripe signin with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,28 @@
 import { GithubLogo, X } from "@phosphor-icons/react"
 import styles from  './styles.module.scss'
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export function SignInBUtton() {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  console.log(session)
+
+  return session ? (
+    <button 
+      className={styles.signInButton} 
+      type="button"
+      onClick={() => signOut()}
+    >
       <GithubLogo color="#04d361"/>
-      Gabi Carniel
+      {session.user.name}
       <X color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button 
+      className={styles.signInButton} 
+      type="button"
+      onClick={() => signIn('github')}
+    >
     <GithubLogo color="#eba417"  />
     Sign in with Github
   </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from "next/app"
 import '../styles/global.scss'
 import { Header } from "../components/Header"
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-  <>
-    <Header/>
-    <Component {...pageProps} />
-   </>
+    <SessionProvider session={session}>
+      <Header/>
+      <Component {...pageProps} />
+    </SessionProvider>
   )
 }
 
